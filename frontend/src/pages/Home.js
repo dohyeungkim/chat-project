6a4547aca54ffdaf13ac2d27144a4652d4360418
김도형 <@.com>
2025-07-12 21:47:06 -0700

    변경

--- frontend/src/pages/Home.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Link } from "react-router-dom";
-export default function Home() {
-    return (_jsxs("div", { style: { padding: "2rem" }, children: [_jsx("h1", { children: "\uCC44\uD305\uBC29 \uC120\uD0DD" }), _jsxs("ul", { children: [_jsx("li", { children: _jsx(Link, { to: "/room/1", children: "\uCC44\uD305\uBC29 1\uBC88" }) }), _jsx("li", { children: _jsx(Link, { to: "/room/2", children: "\uCC44\uD305\uBC29 2\uBC88" }) }), _jsx("li", { children: _jsx(Link, { to: "/room/3", children: "\uCC44\uD305\uBC29 3\uBC88" }) })] })] }));
-}
