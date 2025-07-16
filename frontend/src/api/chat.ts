a52359a65a44153bfc31ca2a948b1d5e54a38c1a
김도형 <@.com>
2025-07-16 13:27:21 -0700

    수정

--- frontend/src/api/chat.ts
+++ frontend/src/api/chat.ts
@@ -1,4 +1,4 @@
-const API_URL = "https://chat-project-1-av9p.onrender.com";
+const API_URL = "https://chat-project-av9p.onrender.com";
 
 export async function fetchMessages(roomId: number) {
   const res = await fetch(`${API_URL}/messages?room_id=${roomId}`);
@@ -19,17 +19,16 @@ export async function sendTextMessage(roomId: number, sender: string, content: s
   return res.json();
 }
 
-export async function sendFileMessage(roomId: number, sender: string, file: File):Promise<string> {
+export async function sendFileMessage(roomId: number, sender: string, file: File) {
   const formData = new FormData();
-  formData.append("file", file);
   formData.append("room_id", String(roomId));
   formData.append("sender", sender);
+  formData.append("type", "file");
+  formData.append("file", file);
+  
 
-  const res = await fetch(`${API_URL}/upload`, {
+  return fetch(`${API_URL}/messages/file/`, {
     method: "POST",
     body: formData,
   });
-
-  const data = await res.json(); // { filename: 'xxxx.mp4' }
-  return data.filename; 
 }
