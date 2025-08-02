ce6bf43255fd3eef9fc9f5f33b28ba5e518725c9
김도형 <@.com>
2025-08-02 01:26:12 -0700

    수정했다

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,8 @@ from app import models
 from app.database import engine
 from app.routes import messages
 from . import rooms                        # rooms 라우터 추가 등록!
-from . import users                        # users 라우터 추가 등록!
+from . import users    # users 라우터 추가 등록!
+from app.websocket import endpoints
 from fastapi.staticfiles import StaticFiles
 
 models.Base.metadata.create_all(bind=engine)
@@ -23,3 +24,4 @@ app.mount("/static", StaticFiles(directory="uploaded_files"), name="static")
 app.include_router(users.router, prefix="/api")
 app.include_router(rooms.router, prefix="/api")
 app.include_router(messages.router, prefix="/api")
+app.include_router(endpoints.router)
